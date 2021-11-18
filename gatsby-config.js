b245ac4bdedca02ca8c55df552407c2b43b7a204
Christoph Uhlich <christoph@familie-uhlich.de>
2021-11-18 21:23:28 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Christoph Uhlich",
+  },
+  plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-react-helmet"],
+};
